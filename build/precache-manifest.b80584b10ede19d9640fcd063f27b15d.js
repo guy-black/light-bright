36fdb449b031a22c0b7609f838bb069961f841bc
guy-black <67166723+guy-black@users.noreply.github.com>
2020-08-15 13:25:48 -0500

    gfg

---
+++ build/precache-manifest.b80584b10ede19d9640fcd063f27b15d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f07fbc94603fd6dbab78abced218944",
+    "url": "/light-bright/index.html"
+  },
+  {
+    "revision": "ce8fefe03d366d42a192",
+    "url": "/light-bright/static/css/main.eb794fda.chunk.css"
+  },
+  {
+    "revision": "19153cbb6114f65914e5",
+    "url": "/light-bright/static/js/2.ac826e15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/light-bright/static/js/2.ac826e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8fefe03d366d42a192",
+    "url": "/light-bright/static/js/main.491ac252.chunk.js"
+  },
+  {
+    "revision": "4f75e731ed73efa30d22",
+    "url": "/light-bright/static/js/runtime-main.f8839e82.js"
+  }
+]);
